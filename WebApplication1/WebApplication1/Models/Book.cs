8e78fededa97b84024428198b0d13a336f704efb
s9011810 <s9011810@gmail.com>
2018-11-14 20:06:25 +0800

    init

---
+++ WebApplication1/WebApplication1/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class BookContect : DbContext {
+
+    }
+    public class Book
+    {
+        public string ID { set; get;}
+        public string NAME { set; get; }
+        public string ISBN { set; get; }
+    }
+}
